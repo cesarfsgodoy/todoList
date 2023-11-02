94f111952df13ee0e014437ce1ce8d40e601918f
César Felipe <cesarfsgodoy@gmail.com>
2023-11-02 16:25:17 -0300

    excluindo linhas desnecessárias

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,7 +1,6 @@
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/pages/auth_page.dart';
-//import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,3 @@
-// import 'dart:math';
-//import 'package:chat/components/messages.dart';
-//import 'package:chat/components/new_message.dart';
-// import 'package:chat/core/models/chat_notification.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/notification_page.dart';
@@ -75,27 +71,6 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
-      // body: SafeArea(
-      //   child: Column(
-      //     children: const [
-      //       Expanded(child: Messages()),
-      //       NewMessage(),
-      //       //Text('teste'),
-      //     ],
-      //   ),
-      // ),
-      // floatingActionButton: FloatingActionButton(
-      //   child: const Icon(Icons.add),
-      //   onPressed: () {
-      //     Provider.of<ChatNotificationService>(
-      //       context,
-      //       listen: false,
-      //     ).add(ChatNotification(
-      //       title: 'Mais uma notificação!',
-      //       body: Random().nextDouble().toString(),
-      //     ));
-      //   },
-      // ),
     );
   }
 }
